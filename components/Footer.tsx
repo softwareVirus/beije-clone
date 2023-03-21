196c58a771a5884494e5f698acd79ce39de002df
softwareVirus <19070001004@stu.yasar.edu.tr>
2023-03-21 18:05:29 +0300

    Edit style object

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Grid, Box } from '@mui/material'
+import { styles } from '@/utils/style'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Grid container>
+      <Box sx={{
+        padding: {
+          md: '80px 144px',
+          xs: '40px 27px'
+        },
+        gap: {
+          md: '64px',
+          xs: '24px'
+        }
+      }}>
+
+      </Box>
+    </Grid>
   )
 }
 
