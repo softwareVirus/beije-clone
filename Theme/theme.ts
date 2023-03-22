802a1c5a622f5b99d1ed832085aa40bc2fc8980f
softwareVirus <19070001004@stu.yasar.edu.tr>
2023-03-22 04:41:15 +0300

    Create display order component for mobile

--- Theme/theme.ts
+++ Theme/theme.ts
@@ -1,45 +1,69 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface TypographyVariants {
+    mobileH2: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    mobileH2?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    mobileH2: true;
+  }
+}
+
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      "Gordita"
-    ].join(","),
+    fontFamily: ["Gordita"].join(","),
     body1: {
-      lineHeight: '1.625rem',
-      letterSpacing: '-0.01em',
-      color: 'rgba(0, 0, 0, 0.9)',
-      cursor: 'pointer',
-      whiteSpace: 'nowrap',
-      verticalAlign: 'middle',
-      height: '26.8px',
+      lineHeight: "1.625rem",
+      letterSpacing: "-0.01em",
+      color: "rgba(0, 0, 0, 0.9)",
+      cursor: "pointer",
+      whiteSpace: "nowrap",
+      verticalAlign: "middle",
+      height: "26.8px",
       hover: {
         borderBottom: "1px solid rgba(0, 0, 0, 0.9)",
         transition: "all 0.5s ease 0s",
-      }
+      },
     },
     h3: {
       margin: 0,
       fontWeight: 500,
-      fontSize: '1.75rem',
-      color: 'rgba(0, 0, 0, 0.9)',
-      lineHeight: '130%',
-      letterSpacing: '-0.0175em'
+      fontSize: "1.75rem",
+      color: "rgba(0, 0, 0, 0.9)",
+      lineHeight: "130%",
+      letterSpacing: "-0.0175em",
     },
     body2: {
       fontWeight: 400,
-      fontSize: '0.875rem',
-      lineHeight: '1.375rem',
-      color: 'rgba(0, 0, 0, 0.6)',
-      display: 'flex'
+      fontSize: "0.875rem",
+      lineHeight: "1.375rem",
+      color: "rgba(0, 0, 0, 0.6)",
+      display: "flex",
     },
     subtitle1: {
       fontWeight: 500,
-      fontSize: '1rem',
-      lineHeight: '160%',
-      letterSpacing: '-0.01em',
-      color: 'rgba(255, 255, 255, 0.698)'
-    }
+      fontSize: "1rem",
+      lineHeight: "160%",
+      letterSpacing: "-0.01em",
+      color: "rgba(255, 255, 255, 0.698)",
+    },
+    mobileH2: {
+      margin:0,
+      fontWeight: 500,
+      fontSize:'1.375rem',
+      fontFamily:'Gordita',
+      lineHeight: '1.625rem',
+      letterSpacing: '-0.03em'
+    },
   },
   breakpoints: {
     values: {
@@ -54,87 +78,87 @@ const theme = createTheme({
     MuiTabs: {
       styleOverrides: {
         scroller: {
-          width: '100%'
+          width: "100%",
         },
         indicator: {
-          backgroundColor:'rgb(52, 49, 49) !important',
-          height:3
-        }
-      }
+          backgroundColor: "rgb(52, 49, 49) !important",
+          height: 3,
+        },
+      },
     },
     MuiTab: {
       styleOverrides: {
         root: {
-          textTransform: 'none',
-          color:'rgba(0, 0, 0, 0.6)',
-          flex: '1 1 0px',
+          textTransform: "none",
+          color: "rgba(0, 0, 0, 0.6)",
+          flex: "1 1 0px",
           fontWeight: 500,
-          letterSpacing: '-0.03em'
-        }
-      }
+          letterSpacing: "-0.03em",
+        },
+      },
     },
     MuiButton: {
       styleOverrides: {
         contained: {
-          textTransform: 'none',
-          width: '100%',
-          lineHeight: '162%',
-          borderRadius: '64px',
-          padding: '12px 22px 10px',
-          fontSize:'1rem'
+          textTransform: "none",
+          width: "100%",
+          lineHeight: "162%",
+          borderRadius: "64px",
+          padding: "12px 22px 10px",
+          fontSize: "1rem",
         },
         text: {
-          borderRadius: '64px',
-          textTransform: 'none',
-          padding: '10px 0px 8px',
-          fontSize: '0.875rem',
-          lineHeight: '171%',
-          letterSpacing: '-0.01em'
-        }
-      }
+          borderRadius: "64px",
+          textTransform: "none",
+          padding: "10px 0px 8px",
+          fontSize: "0.875rem",
+          lineHeight: "171%",
+          letterSpacing: "-0.01em",
+        },
+      },
     },
     MuiTextField: {
       styleOverrides: {
         root: {
-          border: '1px solid rgba(255, 255, 255, 0.23)',
-          borderRadius: '0.5rem !important'
-        }
-      }
+          border: "1px solid rgba(255, 255, 255, 0.23)",
+          borderRadius: "0.5rem !important",
+        },
+      },
     },
     MuiInputBase: {
       styleOverrides: {
         root: {
-          height: '40px !important',
-          borderRadius: '0.5rem !important'
+          height: "40px !important",
+          borderRadius: "0.5rem !important",
         },
         input: {
-          padding: '8.5px 14px !important',
-          color: 'rgba(255, 255, 255, 0.698)'
-        }
-      }
+          padding: "8.5px 14px !important",
+          color: "rgba(255, 255, 255, 0.698)",
+        },
+      },
     },
     MuiInputLabel: {
       styleOverrides: {
         root: {
-          color: 'rgba(255, 255, 255, 0.698) !important',
-          backgroundColor: 'rgb(38, 38, 38)',
+          color: "rgba(255, 255, 255, 0.698) !important",
+          backgroundColor: "rgb(38, 38, 38)",
           //transform: 'translate(14px, 9px) scale(1)'
-        }
-      }
+        },
+      },
     },
     MuiOutlinedInput: {
       styleOverrides: {
         notchedOutline: {
-          color: 'rgba(255, 255, 255, 0.698)'
-        }
-      }
-    }
+          color: "rgba(255, 255, 255, 0.698)",
+        },
+      },
+    },
   },
   palette: {
     primary: {
-      main: 'rgb(52, 49, 49)'
-    }
-  }
+      main: "rgb(52, 49, 49)",
+    },
+  },
 });
 
 export default theme;
--- components/Section/DisplayOrderPopUp.tsx
+++ components/Section/DisplayOrderPopUp.tsx
@@ -1,37 +1,156 @@
 import React, { useContext } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Box, Grid, Typography, IconButton } from "@mui/material";
+import {
+  PadContext,
+  DailyPadContext,
+  TamponContext,
+} from "@/contexts/customContext";
 import Modal from "@mui/material/Modal";
 import { DisplayOrderContext } from "@/contexts/muiComponentContext";
-import DisplayOrder from "./DisplayOrder";
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import { styles } from "@/utils/style";
+import OrderButton from "./display_order/OrderButton";
+import { sizeOfOrder } from "./display_order/calculateCost";
+import ClearIcon from "@mui/icons-material/Clear";
+import InfoSection from "./display_order/InfoSection";
+import Image from "next/image";
+import OrderCard from "./display_order/OrderCard";
 
 export default function DisplayOrderPopUp() {
   const { isDisplayOrderOpen, setIsDisplayOrderOpen } =
     useContext(DisplayOrderContext);
+  const { padInputs, setPadInputs } = useContext(PadContext);
+  const { dailyPadInputs, setDailyPadInputs } = useContext(DailyPadContext);
+  const { tamponInputs, setTamponInputs } = useContext(TamponContext);
+  const { sizeDailyPadOrder, sizePadOrder, sizeTamponOrder } = sizeOfOrder();
+
+  const handleDeletePadOrder = () => {
+    setPadInputs({
+      standardPad: 0,
+      superPad: 0,
+      superPlusPad: 0,
+    });
+  };
+  const handleDeleteDailyPadOrder = () => {
+    setDailyPadInputs({
+      dailyPad: 0,
+      superDailyPad: 0,
+    });
+  };
+
+  const handleDeleteTamponOrder = () => {
+    setTamponInputs({
+      miniTampon: 0,
+      standardTampon: 0,
+    });
+  };
   const handleClose = () => setIsDisplayOrderOpen(false);
 
   return (
     <Modal
       open={isDisplayOrderOpen}
       onClose={handleClose}
+      sx={{
+        overflowY:'scroll'
+      }}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
+      hideBackdrop={false}
     >
-      <Box>
-        
-      </Box>
+      <Grid
+        sx={{
+          ...styles.displayBox,
+          padding: "40px 27px",
+          width: "375px",
+          position: "absolute",
+          top: "54px",
+          gap: "40px",
+          bgcolor: "rgb(247, 246, 245)",
+          right: 0,
+          left: 0,
+          ml: "auto",
+          mr: "auto",
+        }}
+      >
+        <Box
+          sx={{
+            ...styles.displayBox,
+            gap: "1rem",
+          }}
+        >
+          <Grid container justifyContent={"space-between"}>
+            <Typography variant="mobileH2">Özel Paketin</Typography>
+            <IconButton
+              onClick={handleClose}
+              sx={{
+                p: 0,
+              }}
+            >
+              <ClearIcon />
+            </IconButton>
+          </Grid>
+        </Box>
+        <InfoSection />
+        <Image
+          src={
+            "https://beije.co/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpacket.c4ec1969.png&w=640&q=75"
+          }
+          width={402}
+          height={201}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          alt="beije image"
+        />
+        <Box
+          sx={{
+            ...styles.displayBox,
+            width: "100%",
+            gap: "1em",
+            filter:
+              "drop-shadow(rgba(0, 0, 0, 0.03) 0px 1px 3px) drop-shadow(rgba(0, 0, 0, 0.05) 0px 5px 15px)",
+          }}
+        >
+          {sizePadOrder !== 0 ? (
+            <OrderCard
+              title={"Ped Paketleri"}
+              handleFunction={handleDeletePadOrder}
+              order={{ dailyPadInputs, padInputs, tamponInputs }}
+              type={"pad"}
+              sizePadOrder={sizePadOrder}
+              sizeDailyPadOrder={sizeDailyPadOrder}
+              sizeTamponOrder={sizeTamponOrder}
+            />
+          ) : null}
+          {sizeDailyPadOrder !== 0 ? (
+            <OrderCard
+              title={"Günlük Ped Paketleri"}
+              handleFunction={handleDeleteDailyPadOrder}
+              order={{ dailyPadInputs, padInputs, tamponInputs }}
+              type={"dailyPad"}
+              sizePadOrder={sizePadOrder}
+              sizeDailyPadOrder={sizeDailyPadOrder}
+              sizeTamponOrder={sizeTamponOrder}
+            />
+          ) : null}
+          {sizeTamponOrder !== 0 ? (
+            <OrderCard
+              title={"Tampon Paketleri"}
+              handleFunction={handleDeleteTamponOrder}
+              order={{ dailyPadInputs, padInputs, tamponInputs }}
+              type={"tampon"}
+              sizePadOrder={sizePadOrder}
+              sizeDailyPadOrder={sizeDailyPadOrder}
+              sizeTamponOrder={sizeTamponOrder}
+            />
+          ) : null}
+        </Box>
+        <OrderButton
+          padInputs={padInputs}
+          dailyPadInputs={dailyPadInputs}
+          tamponInputs={tamponInputs}
+        />
+      </Grid>
     </Modal>
   );
 }
